73e8a0295491847e8049f10a1d3321f22d97629a
Rainy <652055239@qq.com>
2019-05-23 17:38:50 +0800

    修改代码直接调用代码目录下的mvn来避免需要去下载maven

--- Docs/maven/download.bat
+++ Docs/maven/download.bat
@@ -1,4 +1,4 @@
-call mvn -f pom.xml dependency:copy-dependencies
-call mvn -f pom.xml dependency:sources
-call mvn -f pom.xml dependency:resolve -Dclassifier=javadoc
+call apache-maven-3.6.0\bin\mvn -f pom.xml dependency:copy-dependencies
+call apache-maven-3.6.0\bin\mvn -f pom.xml dependency:sources
+call apache-maven-3.6.0\bin\mvn -f pom.xml dependency:resolve -Dclassifier=javadoc
 @pause
